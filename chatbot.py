5f9e7d3ab889788af7150c6f559c15b213fd0596
mniamulhaque <m.niamul6eg@gmail.com>
2023-12-05 23:46:32 +0600

    first commit

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+multiValu = [2,7,9,8,4,3]
+multiValu.append(3)
+multiValu.append(6)
+multiValu.append("hello")
+# i = 0
+# while i<=5:
+#     print(multiValu[i])
+#     i=i+1
+dic = {"open":"খোলা","What is your name?":"I am Fine","how Old are?":"17 years"}
+for x in dic:
+    vari = input()
+    print(dic[vari])
+
+
+
+
---
+++ main.py
@@ -0,0 +1,30 @@
+#num = int(input("Type any Number: "))
+# if num >= 80:
+#     print("A+")
+# elif num >= 70:
+#     print("A")
+# elif num >= 60:
+#     print("A-")
+# elif num >= 50:
+#     print("B")
+# elif num >= 40:
+#     print("c")
+# elif num >= 33:
+#     print("D")
+# else:
+#     print("Fail")
+# i = 0
+# name = "Bangladeshhjklk"
+# while i <=10:
+#     print(name[i])
+#     i=i+1
+i = 0
+name = [1,7,9,4,3,]
+# while i<=4:
+#     print(name[i])
+#     i=i+1
+for x in name:
+    if x == 4:
+        print(x)
+    elif x == 3:
+        print('No')
